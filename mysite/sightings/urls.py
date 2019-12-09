0af8bcb1d468905c4cc30c1ebb8810b8c8a8248d
Mohanish <mohanishranade@instance-5.c.steam-key-255500.internal>
2019-12-09 21:47:56 +0000

    ;

--- mysite/sightings/urls.py
+++ mysite/sightings/urls.py
@@ -3,4 +3,5 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-            path('', views.index, name='index'),]
+            path('', views.index, name='index'),
+            path('add/', views.add, name='add'),]
--- mysite/sightings/views.py
+++ mysite/sightings/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Sighting
-
+from django.http import HttpResponseRedirect
+from .forms import AddForm
 
 def index(request):
     sq_data = Sighting.objects.all()
@@ -11,5 +12,17 @@ def index(request):
                 'sq_data': sq_data,
                 }
     return HttpResponse(template.render(context, request))
+
+def add(request):
+    if request.method == 'POST':
+        form = AddForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/sightings/')
+    else:
+        form = AddForm()
+
+    return render(request, 'sightings/add.html', {'form':form})
+
 
 # Create your views here.
